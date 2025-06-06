5b7b47f7d1166e3b86a6bfffb7309422751f5424
Pierre Rossel <>
2025-06-06 11:58:48 +0200

    center stars images, fix billboard rotation

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    public float yOffset = 180;
+    public float rotation = 90;
 
     private void OnWillRenderObject()
     {
-        Vector3 targetPos = Camera.main.transform.position;
-        Vector3 lookDir = (targetPos - transform.position);
-        Quaternion lookR = Quaternion.LookRotation(lookDir, Vector3.up);
-        transform.rotation = lookR * Quaternion.Euler(90, yOffset, 0);
+        transform.LookAt(Camera.main.transform.position, Vector3.up);
+        transform.Rotate(rotation, 0, 0);
     }
 }
